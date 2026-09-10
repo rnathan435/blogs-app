f3eb836bd9a845bf0e3e03d9c19bad8f8ac33f89
Rajiv Ravindranathan <rajiv.ravindranathan@gtl.net>
2026-09-09 21:01:03 -0700

    chapter 4 before exercise 13

--- app/actions/blogs.ts
+++ app/actions/blogs.ts
@@ -5,7 +5,11 @@ import { revalidatePath } from "next/cache"
 import { addBlog, likeBlogById } from "../services/blogs"
 import { auth } from "@/auth";
 
-export const createBlog = async (formData: FormData) => {
+export type ActionState = {
+  error?: string
+}
+
+export const createBlog = async (prevState: ActionState, formData: FormData) => {
   const session = await auth()
   if (!session) {
     redirect("/login")
@@ -13,6 +17,19 @@ export const createBlog = async (formData: FormData) => {
   const title = (formData.get("title") as string).trim()
   const author = (formData.get("author") as string).trim()
   const url = (formData.get("url") as string).trim()
+
+  if (!title || title.length < 5) {
+    return { error: "Title must be at least 5 characters long" }
+  }
+
+  if (!author || author.length < 5) {
+    return { error: "Author must be at least 5 characters long" }
+  }
+
+  if (!url || url.length < 5) {
+    return { error: "URL must be at least 5 characters long" }
+  }
+
 
   await addBlog(title, author, url) // likes defaults to 0 in your service
   
